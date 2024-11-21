d4e0c750a80a0fe8bcba02924be1d54ce22de285
RD1119 <rh696@cornell.edu>
2024-11-21 02:42:34 -0500

    update readme

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -292,7 +292,7 @@ def _sum_practice(out: Storage, a: Storage, size: int) -> None:
                 cuda.syncthreads()
             a *= 2
         if pos == 0:
-            out[pos] = cache[0]
+            out[cuda.blockIdx.x] = cache[0]
 
 
 jit_sum_practice = cuda.jit()(_sum_practice)
